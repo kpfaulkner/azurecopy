587c2081d664757103259c02f36cd16f2d64ba7c
Ken Faulkner <ken.faulkner@gmail.com>
2013-05-04 08:17:51 +1000

    sharepoint and dropbox skeleton handlers

---
+++ azurecopy/DropboxHandler.cs
@@ -0,0 +1,56 @@
+﻿﻿//-----------------------------------------------------------------------
+// <copyright >
+//    Copyright 2013 Ken Faulkner
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+// </copyright>
+//-----------------------------------------------------------------------
+
+ using azurecopy.Helpers;
+using azurecopy.Utils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace azurecopy
+{
+    public class DropboxHandler : IBlobHandler
+    {
+        public DropboxHandler()
+        {
+        
+        }
+
+        public Blob ReadBlob(string url, string filePath = "")
+        {
+            throw new NotImplementedException("Dropbox not implemented yet");
+            
+        }
+
+        public void WriteBlob(string url, Blob blob,  int parallelUploadFactor=1, int chunkSizeInMB=4)
+        {
+            throw new NotImplementedException("Dropbox not implemented yet");
+           
+
+        }
+
+        public List<string> ListBlobsInContainer(string container)
+        {
+            throw new NotImplementedException("Dropbox not implemented yet");
+
+        }
+
+    }
+}
