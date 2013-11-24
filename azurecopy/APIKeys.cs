708a47b0963ad484316c9877946c2af62b39137b
Ken Faulkner <ken.faulkner@gmail.com>
2013-11-25 07:59:11 +1100

    test

---
+++ azurecopy/APIKeys.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace azurecopy
+{
+
+    // have a class with built in API keys/secrets etc.
+    // this file should NEVER be checked into github with any data filled in.
+    static class APIKeys
+    {
+        static public string DropBoxAPIKey = "";
+        static public string DropBoxAPISecret = "";
+
+    }
+}
