fa8d51b572b75275de3141db672c13a4c8a37eea
Ken Faulkner <ken.faulkner@gmail.com>
2013-11-24 20:13:10 +1100

    test

--- azurecopy/APIKeys.cs
+++ azurecopy/APIKeys.cs
@@ -10,7 +10,7 @@ namespace azurecopy
     // this file should NEVER be checked into github with any data filled in.
     static class APIKeys
     {
-        static public string DropBoxAPIKey = "d";
+        static public string DropBoxAPIKey = "";
         static public string DropBoxAPISecret = "d";
 
     }
