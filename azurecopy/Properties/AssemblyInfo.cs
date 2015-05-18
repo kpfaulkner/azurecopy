b061adf9d9fdf81a5dc1b81414af367676e8e151
Ken Faulkner <ken.faulkner@gmail.com>
2015-05-19 06:44:28 +1000

    change version to 0.20.0... major changes ahead

--- azurecopy/Properties/AssemblyInfo.cs
+++ azurecopy/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Ken Faulkners")]
 [assembly: AssemblyProduct("azurecopy")]
-[assembly: AssemblyCopyright("Copyright © Ken Faulkner 2014")]
+[assembly: AssemblyCopyright("Copyright © Ken Faulkner 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.16.2")]
-[assembly: AssemblyFileVersion("0.16.2")]
+[assembly: AssemblyVersion("0.20.0")]
+[assembly: AssemblyFileVersion("0.20.0")]
